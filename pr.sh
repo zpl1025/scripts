bd66225e7e761f5bf88fe6cd69e95031be3e042a
Frank Zhang <yellow.zpl@gmail.com>
2015-02-05 21:23:04 +0800

    pr.sh for LCTT

---
+++ pr.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# based on hub
+
+param=$1
+CON="[33m"
+CERR="[31m"
+COFF="[0m"
+
+if [ "$param" != "translating" ]; then
+	if [ "$param" != "translated" ]; then
+		if [ "$param" != "update" ]; then
+			echo "${CERR}[ERR] only support 'translating' 'translated' 'update'${COFF}";
+			exit 1;
+		fi
+	fi
+fi
+
+if [ "$param" == "update" ]; then
+	echo "${CON}[MSG] git fetch lctt${COFF}"
+	git fetch lctt
+	echo "${CON}[MSG] git pull lctt master${COFF}"
+	git pull lctt master
+	echo "${CON}[MSG] Done.${COFF}"
+	exit 0
+fi
+
+count=`git status | grep modified -c`
+if [ "$count" != "1" ]; then
+	echo "${CON}[ERR] not only one modification!${COFF}"
+	git status | grep modified
+	exit 1;
+fi
+
+#cmd="echo git"
+cmd="git"
+
+#modified=`git status | grep modified | cut -d':' -f 2 | xargs`
+modified=`git status | grep modified | cut -d':' -f 2 | sed -e "s/^ *//"`
+if [ "$modified" != "" ]; then
+	if [ ! -f "$modified" ]; then
+		echo "${CERR}[ERR] file not found: \"$modified\" "
+		exit 1
+	fi
+	echo "${CON}[MSG] Updating repository...${COFF}"
+	$cmd fetch lctt 
+	$cmd pull lctt master
+	$cmd push origin master
+
+	filename=`basename "$modified"`
+	echo "${CON}[MSG] Filename: $filename${COFF}"
+	if [ "$param" == "translated" ]; then
+		dstdir=`dirname "$modified" | sed -e "s/sources/translated/"`
+		sed -i "" -e "s/译者ID/zpl1025/g" "$modified"
+		$cmd mv "$modified" "$dstdir"
+	fi
+	echo "${CON}[MSG] Commit to repository...${COFF}"
+	$cmd commit -am "[$param] $filename" 
+	$cmd push origin master 
+	echo "${CON}[MSG] Create pull request...${COFF}"
+	if [ "$cmd" == "git" ]; then
+		hub pull-request -m "[$param] $filename" -b LCTT:master
+	else
+		echo hub pull-request -m "[$param] $filename" -b LCTT:master
+	fi
+	echo "${CON}[MSG] Done.${COFF}"
+fi
+
+# # add remote repo alias (the default alias for remote repo when cloned is origin)
+# git remote add lctt https://github.com/LCTT/TranslateProject.git  
+# 
+# # sync the upstream repo
+# git fetch lctt
+# 
+# # merge the upstream repo to mine
+# git pull lctt master
+# 
+# # make your changes
+# 
+# # commit changed to your remote
+# git push origin master
+# 
+# # invoke pull request on github.com
+# # git pull-request [-o|--browse] [-f] [-m MESSAGE|-F FILE|-i ISSUE|ISSUE-URL] [-b BASE] [-h HEAD]
+# # BASE/HEAD: branch, owner:branch, owner:repo/branch
+# hub pull-request -m "Implemented feature X" -b LCTT:master -h mislav:feature
